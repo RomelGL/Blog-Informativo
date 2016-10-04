7de85c6e102ebb07abc7354b1d9e1915996a9dad
Romel <romel_174@hotmail.com>
2016-10-04 13:57:51 -0400

    Index.php Estructura Basica - Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Blog</title>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/estilos.css">
+
+</head>
+<body>
+
+
+<script src="../jquery/jquery-3.1.0.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+
+</body>
+</html>
